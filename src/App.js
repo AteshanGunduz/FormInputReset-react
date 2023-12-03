9c82d42b320163f3a938aa4353aeca3ef9fa45ab
AteshanGunduz <145977486+AteshanGunduz@users.noreply.github.com>
2023-12-03 13:01:41 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+export default function Form() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  function handleFirstNameChange(e) {
+    setFirstName(e.target.value);
+  }
+
+  function handleLastNameChange(e) {
+    setLastName(e.target.value);
+  }
+
+  function handleReset() {
+    setFirstName("");
+    setLastName("");
+  }
+  return (
+    <form onSubmit={(e) => e.preventDefault()}>
+      <input
+        placeholder="Name"
+        value={firstName}
+        onChange={handleFirstNameChange}
+      />
+      <input
+        placeholder="Surname"
+        value={lastName}
+        onChange={handleLastNameChange}
+      />
+      <h1>
+        Hi, {firstName} {lastName}
+      </h1>
+      <button onClick={handleReset}>Reset</button>
+    </form>
+  );
+}
